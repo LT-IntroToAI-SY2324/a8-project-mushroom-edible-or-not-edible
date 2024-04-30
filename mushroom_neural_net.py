9e326b9213a6db10b08b14baf05c2c4dcb2e9e19
Catherine Markowska <cimarkowska@cps.edu>
2024-04-30 10:33:07 -0500

    worked on neural net

--- mushroom_neural_net.py
+++ mushroom_neural_net.py
@@ -5,15 +5,15 @@ from typing import Tuple
 from neural import *
 from sklearn.model_selection import train_test_split
 
-def parse_line(line: str) -> Tuple[List[float], List[float]]:
+def parse_line(line: str) -> Tuple[List[str], List[str]]:
     tokens = line.split(",")
-    out = int(tokens[0])
+    out = str(tokens[0])
     output = [0 if out == 1 else 0.5 if out == 2 else 1]
 
     inpt = [float(x) for x in tokens[1:]]
     return (inpt, output)
 
-def normalize(data: List[Tuple[List[float], List[float]]]):
+def normalize(data: List[Tuple[List[str], List[str]]]):
     leasts = len(data[0][0]) * [100.0]
     mosts = len(data[0][0]) * [0.0]
 
