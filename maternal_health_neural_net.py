a97f2d32275d507e7d7f5a73ee0507ec9d0b954d
Catherine Markowska <cimarkowska@cps.edu>
2024-05-01 10:42:23 -0500

    switced dataset to maternal health

--- maternal_health_neural_net.py
+++ maternal_health_neural_net.py
@@ -1,19 +1,39 @@
 # Cat Markowska, P.3
 # Kelly Ocampo, P.3
+# data: Age,SystolicBP,DiastolicBP,BS,BodyTemp,HeartRate
 
 from typing import Tuple
 from neural import *
 from sklearn.model_selection import train_test_split
 
-def parse_line(line: str) -> Tuple[List[str], List[str]]:
+
+def parse_line(line: str) -> Tuple[List[float], List[float]]:
+    """Splits line of CSV into inputs and output (transormfing output as appropriate)
+
+    Args:
+        line - one line of the CSV as a string
+
+    Returns:
+        tuple of input list and output list
+    """
     tokens = line.split(",")
-    out = str(tokens[0])
+    out = int(tokens[0])
     output = [0 if out == 1 else 0.5 if out == 2 else 1]
 
     inpt = [float(x) for x in tokens[1:]]
     return (inpt, output)
 
-def normalize(data: List[Tuple[List[str], List[str]]]):
+
+def normalize(data: List[Tuple[List[float], List[float]]]):
+    """Makes the data range for each input feature from 0 to 1
+
+    Args:
+        data - list of (input, output) tuples
+
+    Returns:
+        normalized data where input features are mapped to 0-1 range (output already
+        mapped in parse_line)
+    """
     leasts = len(data[0][0]) * [100.0]
     mosts = len(data[0][0]) * [0.0]
 
@@ -29,18 +49,23 @@ def normalize(data: List[Tuple[List[str], List[str]]]):
             data[i][0][j] = (data[i][0][j] - leasts[j]) / (mosts[j] - leasts[j])
     return data
 
-with open("mushroom_data.txt", "r") as f:
+
+with open("mathernal_health_risk.txt", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
 
 # print(training_data)
+
 td = normalize(training_data)
-# print(td)
+print(len(td))
 
 train, test = train_test_split(td)
+print(len(train))
+print(len(test))
+print(test)
 
 nn = NeuralNet(13, 3, 1)
-nn.train(train, iters=10000, print_interval=1000, learning_rate=0.2)
+nn.train(train, iters=10000, print_interval=1000, learning_rate=0.1)
 
-for i in nn.test_with_expected(test):
+for i in nn.test_with_expected(td):
     difference = round(abs(i[1][0] - i[2][0]), 3)
-    print(f"desired: {i[1]}, actual: {i[2]} diff: {difference}")
+    print(f"desired: {i[1]}, actual: {i[2]}, diff: {difference}")
